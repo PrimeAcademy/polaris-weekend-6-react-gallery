81a472da14b8c85e2b6d78471bc8d6498d3f46ce
Kris Szafranski <kris@thoughtshelter.com>
2018-09-04 09:43:54 -0500

    added photo variety. filled out image data on server

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/luca-incredibles.jpg', description: 'Am I needed somewhere?', likes: 0 },
+    { id: 2, path: 'images/luca-breakfast.jpg', description: 'Luca is hilarious and breakfast. What a ham.', likes: 2 },
+    { id: 3, path: 'images/nora-sleeping.jpg', description: 'What a bum.', likes: 3 },
+    { id: 4, path: 'images/gretzky.jpg', description: 'The Great One is one of my inspirational heroes.', likes: 0 },
+    { id: 5, path: 'images/add2.png', description: `A Druid's Duel is my only completed and released game.`, likes: 0 },
+    { id: 6, path: 'images/luca-jess.jpg', description: 'My Loves', likes: 0 },
 ];
 
 module.exports = galleryItems;
