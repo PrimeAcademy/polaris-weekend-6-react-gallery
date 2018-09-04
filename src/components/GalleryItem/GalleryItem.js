c305e387da15b0b10e500d4dc479e402398ad651
Kris Szafranski <kris@thoughtshelter.com>
2018-09-04 10:52:29 -0500

    updated image path, changed text

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -5,7 +5,8 @@ class GalleryItem extends Component {
     render() {
         return (
             <div className="galleryItem">
-                <img src="images/luca-incredibles.jpg" alt="Luca" />
+                <img src={this.props.itemInfo.path} 
+                     alt={this.props.itemInfo.description} />
                 <button className="likeButton"><i className="far fa-thumbs-up"></i></button>
             </div>
         );
