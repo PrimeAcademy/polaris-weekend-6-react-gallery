18012a1bca7556f3b29af523ee1bc09d2b6c8851
Kris Szafranski <kris@thoughtshelter.com>
2018-09-04 10:58:55 -0500

    moved gallery items to app component

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -4,23 +4,12 @@ import GalleryItem from '../GalleryItem/GalleryItem.js';
 class GalleryList extends Component {
 
   constructor() {
-    super();
-    // state data. Will eventually come from this.props
-    this.state = {
-      galleryItems: [
-        { id: 1, path: 'images/luca-incredibles.jpg', description: 'Am I needed somewhere?', likes: 0 },
-        { id: 2, path: 'images/luca-breakfast.jpg', description: 'Luca is hilarious and breakfast. What a ham.', likes: 2 },
-        { id: 3, path: 'images/nora-sleeping.png', description: 'Sleeping baby is sleepy.', likes: 3 },
-        { id: 4, path: 'images/gretzky.jpg', description: 'The Great One is one of my inspirational heroes.', likes: 0 },
-        { id: 5, path: 'images/add2.png', description: `A Druid's Duel is my only completed and released game.`, likes: 0 },
-        { id: 6, path: 'images/luca-jess.jpg', description: 'My Loves', likes: 0 },
-      ],
-    };
+    super();    
   }
 
   render() {
     // do some mappin'
-    const data = this.state.galleryItems.map( (item) => {
+    const data = this.props.galleryItems.map( (item) => {
       return <GalleryItem key={item.id} itemInfo={item} />;
     });
 
